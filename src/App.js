a616dc1c312243266d1bbb8e6e4fbb18071e11b7
Juan Ortega <juanalbertoortega456@gmail.com>
2021-07-24 11:13:59 -0500

    Basic form created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
 function App() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="example@a.com" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+      </div>
+      <div>
+          <label htmlFor="data">File</label>
+          <input type="file" {...register("data", { required: true })} />
+      </div>
+      <button>Submit</button>
+    </form>
+
   );
 }
 
